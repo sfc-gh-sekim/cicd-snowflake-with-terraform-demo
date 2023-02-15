a4b5c18ec043b29c3b78d448043badb7458e4731
seankimthesnowflake <sean.kim@snowflake.com>
2023-02-16 08:02:30 +1100

    main.tf

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cicd-snowflake-with-terraform-demo
 Demo of a CI/CD pipeline deployed onto Snowflake with Terraform
+
+## Steps
+
+Install Terraform. On OSX, we recommend Homebrew:
+
+```
+brew tap hashipcorp/tap
+brew install hashicorp/tap/terraform
+```
---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.56.3"
+    }
+  }
+
+  backend "remote" {
+    organization = "sean-kim-demo-corp"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
