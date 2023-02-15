f780e753213f6a94dba468dfa6889547af1809d9
seankimthesnowflake <sean.kim@snowflake.com>
2023-02-16 10:07:45 +1100

    fmt

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
 terraform {
-    required_providers {
-        snowflake = {
-            source  = "Snowflake-Labs/snowflake"
-            version = "0.56.3"
-        }
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "0.56.3"
     }
+  }
 
-    backend "remote" {
-        organization = "sean-kim-demo-corp"
+  backend "remote" {
+    organization = "sean-kim-demo-corp"
 
-        workspaces {
-            name = var.ENVIRONMENT
-        }
+    workspaces {
+      name = var.ENVIRONMENT
     }
+  }
 }
 
 provider "snowflake" {
 }
 
 resource "snowflake_database" "terraform_demo_db" {
-    name    = "TERRAFORM_DEMO_DB_${terraform.workspace}"
-    comment = "Database for Snowflake Terraform demo"
+  name    = "TERRAFORM_DEMO_DB_${terraform.workspace}"
+  comment = "Database for Snowflake Terraform demo"
 }
 
 resource "snowflake_schema" "terraform_demo_schema" {
-    name = snowflake_database.terraform_demo_db.name
-    comment = "Schema for Snowflake Terraform demo"
+  name    = snowflake_database.terraform_demo_db.name
+  comment = "Schema for Snowflake Terraform demo"
 }
 
