80ebc44fe26ef6bfb1aa87d0a81c4664ccb15a66
seankimthesnowflake <sean.kim@snowflake.com>
2023-05-24 19:39:56 +1000

    upsize compute clusters

--- modules/snowflake_resources/db_schema.tf
+++ modules/snowflake_resources/db_schema.tf
@@ -1,15 +1,6 @@
 resource "snowflake_database" "tf_demo_database" {
-  name = var.database
-}
-
-## Set data retention period on DB
-resource "snowflake_object_parameter" "o" {
-  key         = "DATA_RETENTION_TIME_IN_DAYS"
-  value       = var.time_travel_in_days
-  object_type = "DATABASE"
-  object_identifier {
-    name = snowflake_database.tf_demo_database.name
-  }
+  name                        = var.database
+  data_retention_time_in_days = var.time_travel_in_days
 }
 
 resource "snowflake_schema" "tf_demo_schema" {
--- modules/snowflake_resources/warehouses.tf
+++ modules/snowflake_resources/warehouses.tf
@@ -1,6 +1,6 @@
 resource "snowflake_warehouse" "task_warehouse" {
   name           = var.env_name == "PROD" ? "VHOL_TASK_WAREHOUSE" : "VHOL_TASK_WAREHOUSE_${var.env_name}"
-  warehouse_size = var.env_name == "PROD" ? "MEDIUM" : "XSMALL"
+  warehouse_size = var.env_name == "PROD" ? "LARGE" : "SMALL"
   auto_resume    = true
   auto_suspend   = 1
 }
