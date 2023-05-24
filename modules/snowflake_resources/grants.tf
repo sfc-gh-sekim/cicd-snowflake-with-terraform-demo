2f168d2af130193e1989c11a79f7871f93085633
seankimthesnowflake <sean.kim@snowflake.com>
2023-05-24 11:53:52 +1000

    fix grants

--- modules/snowflake_resources/grants.tf
+++ modules/snowflake_resources/grants.tf
@@ -2,7 +2,7 @@ resource "snowflake_database_grant" "database_ro_grant" {
   database_name = snowflake_database.tf_demo_database.name
 
   privilege = "USAGE"
-  roles     = ["TF_DEMO_READER"]
+  roles     = [snowflake_role.tf_demo_reader_role]
 }
 
 resource "snowflake_schema_grant" "schema_ro_grant" {
@@ -10,7 +10,7 @@ resource "snowflake_schema_grant" "schema_ro_grant" {
   schema_name   = snowflake_schema.tf_demo_schema.name
 
   privilege = "USAGE"
-  roles     = ["TF_DEMO_READER"]
+  roles     = [snowflake_role.tf_demo_reader_role]
 }
 
 resource "snowflake_table_grant" "table_ro_grant" {
@@ -18,7 +18,7 @@ resource "snowflake_table_grant" "table_ro_grant" {
   schema_name   = snowflake_schema.tf_demo_schema.name
 
   privilege = "SELECT"
-  roles     = ["TF_DEMO_READER"]
+  roles     = [snowflake_role.tf_demo_reader_role]
 
   on_future         = false
   with_grant_option = false
@@ -30,7 +30,7 @@ resource "snowflake_view_grant" "view_ro_grant" {
   schema_name   = snowflake_schema.tf_demo_schema.name
 
   privilege = "SELECT"
-  roles     = ["TF_DEMO_READER"]
+  roles     = [snowflake_role.tf_demo_reader_role]
 
   on_future         = false
   with_grant_option = false
